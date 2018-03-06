f5054f12e029e6639ff5958776496cf9042ed4e5
Hannah Lee <hannahlee@mymail.mines.edu>
2018-03-05 17:49:01 -0700

    changed score from 90 to 85

--- src/TheMotivator.java
+++ src/TheMotivator.java
@@ -2,7 +2,7 @@ public class TheMotivator {
 	public void feedback(int score) {
 		if (score == 100)
 			System.out.println("You're awesome");
-		else if (score > 90)
+		else if (score > 85)
 			System.out.println("That's great");
 		else if (score > 60)
 			System.out.println("That's okee");
